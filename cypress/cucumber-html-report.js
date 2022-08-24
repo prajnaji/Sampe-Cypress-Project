088426d23a53ce21459e85a913b0fb61e3a11aa4
prajnaji <prajna4mohanty@gmail.com>
2022-08-24 17:57:10 +0530

    Report Generation

---
+++ cypress/cucumber-html-report.js
@@ -0,0 +1,21 @@
+// //Let name it cucumber-html-report.js **
+// const report = require("multiple-cucumber-html-reporter");
+// report.generate({
+// jsonDir: "./cypress/cucumber-json/",  // ** Path of .json file **//
+// reportPath: "./reports/cucmber-htmlreport.html",
+// metadata: {
+// browser: {
+// name: "chrome",
+// version: "104",
+// },
+// device: "Local test machine",
+// platform: {
+// name: "hp",
+// version: "Windows",
+// },
+// },
+// });
+
+
+//----------------->>>>>>>>>>>
+

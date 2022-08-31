8d3722917a702fbf7d333d757ab01c1c3de5e232
prajnaji <prajna4mohanty@gmail.com>
2022-08-31 08:14:02 +0530

    Delivery Details POM file added

---
+++ cypress/support/pageObjects/DeliveryDetails.cy.js
@@ -0,0 +1,26 @@
+class DeliveryDetails{
+
+    /*First & Last Name field */
+    nameField(){ return cy.get('input[name=fname]');};
+
+    /*Address Line 1*/
+    addressLine1(){return cy.get('input[name=saddr]')};
+
+    /*AddressLine 2 */
+    addressLine2(){return cy.get('input[name=saddr2]')};
+     
+    /*mobile number 1 */
+    mobileNumber1(){return cy.get('input[name=mob]')};
+
+    /*Address Type Radio Buttons */
+    addressTypeRadioBtns(){return cy.get('div.with-otherfield-addr')}
+
+    /*Save & Submit Button */
+    submitButton(){return cy.get('button.btn.revamp-c-save-addr-btn');}
+
+
+
+    
+   
+}
+export default DeliveryDetails;
